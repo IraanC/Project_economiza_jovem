e29e3d2f4c71fda5f5819a5eb029a23abe3f2f5a
Deivid Souza <deividsouza252@gmail.com>
2024-11-07 23:59:33 -0300

    Atualizando a página inicial.

--- js/script.js
+++ js/script.js
@@ -7,35 +7,34 @@ menuToggle.addEventListener('click', () => {
 });
 // fim animação mob //
 
-// animação das imagens
-let currentSlide = 0;
-const slides = document.querySelectorAll('.slide');
-const textBoxes = document.querySelectorAll('.text-box');
+let currentIndex = 0;
 
 function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.classList.toggle('active', i === index);
-    });
-    updateTextBoxes(index);
-    currentSlide = index; // Atualiza o slide atual
-}
+    const slides = document.querySelectorAll('.carousel-item');
+    if (index >= slides.length) {
+        currentIndex = 0;
+    } else if (index < 0) {
+        currentIndex = slides.length - 1;
+    } else {
+        currentIndex = index;
+    }
 
-function updateTextBoxes(index) {
-    textBoxes.forEach((textBox, i) => {
-        textBox.textContent = `Slide ${i + 1}`; // Atualiza o texto da caixa para o número do slide
-        textBox.classList.remove('active'); // Remove a classe active de todas as caixas
+    const carouselInner = document.querySelector('.carousel-inner');
+    carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
+
+    slides.forEach((slide, i) => {
+        slide.classList.toggle('active', i === currentIndex);
     });
-    textBoxes[index].classList.add('active'); // Adiciona a classe active apenas ao slide ativo
 }
 
 function nextSlide() {
-    currentSlide = (currentSlide + 1) % slides.length; // Avança para o próximo slide
-    showSlide(currentSlide);
+    showSlide(currentIndex + 1);
 }
 
-// Exibir o slide inicial
-showSlide(currentSlide);
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
 
-// Muda automaticamente os slides a cada 5 segundos
-setInterval(nextSlide, 5000);
-// fim do cod da animação //
+document.addEventListener('DOMContentLoaded', () => {
+    showSlide(currentIndex);
+});
