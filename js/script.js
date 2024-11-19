81014dd0fe1bb1bf012ee7ec002e26a0faa22737
Ismael Rodrigues <Ismaelneto2003@gmail.com>
2024-11-19 12:44:51 -0300

    tirando script comentado

--- js/script.js
+++ js/script.js
@@ -1,35 +1,3 @@
-// let currentIndex = 0;
-
-// function showSlide(index) {
-//     const slides = document.querySelectorAll('.carousel-item');
-//     if (index >= slides.length) {
-//         currentIndex = 0;
-//     } else if (index < 0) {
-//         currentIndex = slides.length - 1;
-//     } else {
-//         currentIndex = index;
-//     }
-
-//     const carouselInner = document.querySelector('.carousel-inner');
-//     carouselInner.style.transform = `translateX(-${currentIndex * 100}%)`;
-
-//     slides.forEach((slide, i) => {
-//         slide.classList.toggle('active', i === currentIndex);
-//     });
-// }
-
-// function nextSlide() {
-//     showSlide(currentIndex + 1);
-// }
-
-// function prevSlide() {
-//     showSlide(currentIndex - 1);
-// }
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     showSlide(currentIndex);
-// });
-
 /* script.js */
 
 let count = 1;
